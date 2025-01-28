5ba71da3e35f0adbd52eb1a013289c209cae78b4
Douglas Rangel <douglascoutinho202@gmail.com>
2025-01-28 19:30:43 -0300

    create a new micro frontend

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,24 +1,10 @@
-import { registerApplication, start, LifeCycles } from "single-spa";
+import { LifeCycles, registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    import(
-      /* webpackIgnore: true */ // @ts-ignore-next
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@home-hub/react-navbar",
+  app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
   activeWhen: ["/"],
 });
-
-// registerApplication({
-//   name: "@home-hub/navbar",
-//   app: () =>
-//     import(
-//       /* webpackIgnore: true */ // @ts-ignore-next
-//       "@home-hub/navbar"
-//     ),
-//   activeWhen: ["/"],
-// });
 
 start({
   urlRerouteOnly: true,
